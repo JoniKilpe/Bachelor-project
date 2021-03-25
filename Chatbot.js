b627b3b5fe32ff23af06fc0f65f26aa8d2a963e4
akanerva <aleksi.kanerva90@gmail.com>
2021-03-25 19:09:53 +0200

    syntaksi fiksattu ja listatty testi

--- Chatbot.js
+++ Chatbot.js
@@ -6,38 +6,67 @@
 class Chatbot {
     // konstruktoriin todnäk pitää syöttää joko koko projektiolio tai ainakin
     // tietyt komponentit siitä
-    constructor(project) { // init vs constructor??? pls send help
-        this.project = project;
-        this.contextVariables = {}; // jotain json-kikkailua tähän attribuuttiin?
+    constructor() { // init vs constructor??? pls send help
+        //this.project = project;
+        //this.contextVariables = {}; // jotain json-kikkailua tähän attribuuttiin?
 
         const AssistantV2 = require('ibm-watson/assistant/v2');
         const { IamAuthenticator } = require('ibm-watson/auth');
 
-        this.watson = new AssistantV2({
+        this.assistant = new AssistantV2({
             version: '2020-09-24',
             authenticator: new IamAuthenticator({
-                apikey: '', //otetaankohan nämä .env-tiedostosta?
+                apikey: 'o3x3yQtjonJlDk_68JVqI7xrRwYZHKZdcpxyfTwTP0oZ', //otetaankohan nämä .env-tiedostosta?
             }),
-            url: '', //otetaankohan nämä .env-tiedostosta?
+            url: 'https://api.eu-de.assistant.watson.cloud.ibm.com/instances/1b317c60-a115-4aa5-bffa-87bb218044d0', //otetaankohan nämä .env-tiedostosta?
         });
 
-        const assistantId = ''; //otetaankohan nämä .env-tiedostosta?
+        this.assistantId = '7184ad88-be61-482d-90b7-e3fac855baa7'; //otetaankohan nämä .env-tiedostosta?
     }
 
     //Tuomon antama esimerkkifunktio. mahdollisesti toteutetaan eri tavalla
-    function startChat(project, params, callback) {
-
+    startChat(project, params, callback) {
 
         //callback();
     }
 
     //node sdk copypastella pääsee varmaan suhteellisen pitkälle
-    function sendMessage(input) {
+    sendMessage(input) {
+        this.assistant
+          .messageStateless({
+            assistantId: this.assistantId,
+            input: {
+              'message_type': 'text',
+              'text': input,
+            }
+          })
+          .then(res => {
+            this.processResponse(res.result);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+    }
 
+    processResponse(response) {
+        if (response.output.generic) {
+          if (response.output.generic.length > 0) {
+            if (response.output.generic[0].response_type === 'text') {
+              console.log(response.output.generic[0].text);
+              }
+            }
+          }
     }
 
     //kutsutaan pelin omaa laskufunktiota
-    function calculateResult() {
+    calculateResult() {
 
     }
 }
+
+//testaus
+
+var chatbot = new Chatbot();
+var viesti = '';
+
+chatbot.sendMessage(viesti);
