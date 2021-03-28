e9ce4c9b6d6ae2fce5421de199f52860703c4cb4
akanerva <aleksi.kanerva90@gmail.com>
2021-03-28 05:09:50 +0300

    vähän eri testi, mutta pari uutta/täydennettyä funktiota, joten tästä on hyvä jatkaa

--- Chatbot.js
+++ Chatbot.js
@@ -1,19 +1,12 @@
 //importit - todnäk UI:sta ainakin jotain
-
-
-//konstruktoriin parametrit
+require("dotenv").config();
 
 class Chatbot {
     // konstruktoriin todnäk pitää syöttää joko koko projektiolio tai ainakin
     // tietyt komponentit siitä
-    constructor() { // init vs constructor??? pls send help
-        //this.project = project;
-        //this.contextVariables = {}; // jotain json-kikkailua tähän attribuuttiin?
-
+    constructor() {
         const AssistantV2 = require('ibm-watson/assistant/v2');
         const { IamAuthenticator } = require('ibm-watson/auth');
-        this.contextVariables = [];
-
         this.assistant = new AssistantV2({
             version: '2020-09-24',
             authenticator: new IamAuthenticator({
@@ -21,11 +14,14 @@ class Chatbot {
             }),
             url: process.env.WATSON_ASSISTANT_URL,
         });
-
-        this.assistantId = process.env.WATSON_ASSISTANT_ID; //'7184ad88-be61-482d-90b7-e3fac855baa7'
+        this.assistantId = process.env.WATSON_ASSISTANT_ID;
+        this.contextVariables = {
+            "hyvat_arvot":true
+        }
+        this.sessionId = null;
     }
 
-    //node sdk copypastella pääsee varmaan suhteellisen pitkälle
+    //TODO: promise
     sendMessage(input) {
         this.assistant
           .messageStateless({
@@ -43,7 +39,54 @@ class Chatbot {
           });
     }
 
+    createSession() {
+        //promise auttaa flow controllissa, eli siinä että saa ohjelman
+        //suorittamaan asiat oikeassa järjestyksessä ylhäältä alas
+        return new Promise((resolve, reject) => {
+            this.assistant.createSession({
+                assistantId: this.assistantId
+            })
+            .then(res => {
+                this.sessionId = res.result.session_id;
+                console.log(res.result.session_id);
+                return resolve();
+                //console.log(JSON.stringify(res.result, null, 2));
+            })
+            .catch(err => {
+                console.log(err);
+                return reject();
+            });
+        })
+
+
+    }
+
+    //TODO: promise (onko täysin tarpeellinen?)
+    deleteSession() {
+        this.assistant.deleteSession({
+            assistantId: this.assistantId,
+            sessionId: this.sessionId
+        })
+        .then(res => {
+            console.log("session deleted"); //debuggausta varten
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    getSessionId() {
+        return this.sessionId;
+    }
+
+    //TODO: testaa! pitäisi appendaa json-objektiin uuden avain-arvo -parin
+    addContextVariable(property, value) {
+        this.contextVariables[property] = value;
+    }
+
+    //TODO: promise
     processResponse(response) {
+        //console.log(JSON.stringify(response, null, 2));
         if (response.output.generic) {
           if (response.output.generic.length > 0) {
             if (response.output.generic[0].response_type === 'text') {
@@ -58,9 +101,14 @@ class Chatbot {
 
     }
 }
-//testaus
 
+// testaus - tällä hetkellä luo session, printtaa session_id:n, ja deletoi session
 var chatbot = new Chatbot();
-var viesti = '';
 
-chatbot.sendMessage(viesti);
+//ohjelma odottaa createSessionin loppuun viemistä
+//ennen deleteSessioniin jatkamista
+chatbot.createSession()
+    .then((result) => {
+        chatbot.deleteSession();
+    })
+    //TODO: catch?
