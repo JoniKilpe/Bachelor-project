e11e713b8c6e955dc2a099b11cbd19e0b58ecce8
akanerva <aleksi.kanerva90@gmail.com>
2021-04-14 20:53:28 +0300

    react+nodejs-versio

---
+++ reactversio/projreact/server.js
@@ -0,0 +1,18 @@
+//import dependencies
+const express = require('express');
+const app = express();
+require("dotenv").config();
+
+//allow parsing on request bodies
+app.use(express.json());
+
+//import routes for api
+const watsonRoutes = require("./routes/api/watson")
+//direct requests to api/watson to Watson Routes
+app.use("/api/watson", watsonRoutes);
+//start server
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("Server listening on port ", port);
+    console.log("Hello world!");
+})
